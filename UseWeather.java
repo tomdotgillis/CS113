346d7d537bbf025b0e69bbafd2ab6815208d75eb
tomdotgillis <Tomdotgillis@gmail.com>
2022-02-20 02:36:36 -0500

    Final version ready to submit
    Working and commented final versions of the instantiable and client classes

--- UseWeather.java
+++ UseWeather.java
@@ -2,16 +2,25 @@
 ///////////////////////////////////////////////////////
 // Name: Thomas Gillis
 // 
-// Date: 1/18/2
+// Date: 2/22/22
 //
 // Methods: 
-//      main(String[] args) - void - utilizes the Weather 
+//      + main(String[] args) - void - utilizes the Weather 
 //                            class to find various data 
 //                            about the weather in Burlington, 
 //                            Vermont over a specific time.
 // 
+//      - isInvalidResponseYear() - int - asks the user  to provide
+//                                        enter a valid year
 //
-// 
+//      - isInvalidResponseMonth() - int - asks the user to create
+//                                         enter a valid month (1-12)
+//
+//      - inRangeYear() - boolean -  returns true if the user 
+//                                   provided a valid year
+//
+//      - inRangeMonth() - boolean - returns true if the user
+//                                   provided a valid month
 //
 ///////////////////////////////////////////////////////
 
@@ -21,12 +30,24 @@ import javax.swing.JOptionPane;
 import java.util.StringTokenizer;
 
 public class UseWeather {
+        public static final int YEAR_START = 1970; // starting year for this specific data set
+        public static final int YEAR_END = 1989; // ending year for this specific data set
+        public static final int MONTH_START = 1; // starting month for this specific data set
+        public static final int MONTH_END = 12; // ending month for this specific data set
+
         public static void main(String[] args) throws Exception {
-                double[] monthlyRain, monthlySnow, yearlyRain, yearlySnow;
-                int start, end, month;
-                String inputString;
-                int input;
+                double[] monthlyRain, // array that holds te necessary amount of doubles - monthlyRain()
+                                monthlySnow, // array that holds te necessary amount of doubles - monthlySnow()
+                                yearlyRain, // array that holds te necessary amount of doubles - yearlyRain()
+                                yearlySnow, // array that holds te necessary amount of doubles - yearlySnow()
+                                anualHighTemp;// array that holds te necessary amount of doubles - anualHighTemp()
+                int start, // year to START searching
+                                end, // year to STOP searhing
+                                month;// month to specifically search for
+                String inputString; // String representation of the next line in the data file being input
+                int input;// integer representation of the next datapoint in the string 'inputString'
                 Weather weather = new Weather();
+                boolean isInvalidResponse = false;
                 String[] monthStrings = { "JANUARY", "FEBRUARY", "MARCH", "APRIL", "MAY", "JUNE", "JULY", "AUGUST",
                                 "SEPTEMBER",
                                 "OCTOBER", "NOVEMBER", "DECEMBER" };
@@ -37,23 +58,40 @@ public class UseWeather {
 
                 // Monthly Rainfall Data
                 if (input == 1) {
+                        // ask user for next input
                         inputString = JOptionPane.showInputDialog(null,
                                         "What year to start?",
                                         "Weather Data", JOptionPane.QUESTION_MESSAGE);
                         start = Integer.parseInt(inputString);
 
+                        // is valid year
+                        while (!inRangeYear(start))
+                                start = isInvalidResponseMonth();
+
+                        // ask user for next input
                         inputString = JOptionPane.showInputDialog(null,
                                         "What year to end?",
                                         "Weather Data", JOptionPane.QUESTION_MESSAGE);
                         end = Integer.parseInt(inputString);
+
+                        // is valid year
+                        while (inRangeYear(end) == false)
+                                end = isInvalidResponseMonth();
+
+                        // ask user for next input
                         inputString = JOptionPane.showInputDialog(null,
                                         "What month to look at?\n[1-12] Jan-Dec",
                                         "Weather Data", JOptionPane.QUESTION_MESSAGE);
                         month = Integer.parseInt(inputString);
+
+                        // is valid month
+                        while (inRangeMonth(month) == false)
+                                month = isInvalidResponseMonth();
+
                         monthlyRain = weather.monthlyRainfall(start, end, month);
                         System.out.println("- -\n" + monthStrings[month - 1] + "\n- -");
                         for (int i = 0; i < monthlyRain.length; i++) {
-                                System.out.println("RAIN AVERAGE FOR " + monthStrings[month - 1] + " IN " + (start + i)
+                                System.out.println("RAINFALL FOR " + monthStrings[month - 1] + " IN " + (start + i)
                                                 + ": "
                                                 + String.format("%.3f", monthlyRain[i]));
                         }
@@ -61,40 +99,69 @@ public class UseWeather {
 
                 // Monthly Snowfall Data
                 else if (input == 2) {
+
+                        // ask user for next input
                         inputString = JOptionPane.showInputDialog(null,
                                         "What year to start?",
                                         "SNOWFALL Data", JOptionPane.QUESTION_MESSAGE);
                         start = Integer.parseInt(inputString);
 
+                        // is valid input
+                        while (!inRangeYear(start))
+                                start = isInvalidResponseYear();
+
+                        // ask user for next input
                         inputString = JOptionPane.showInputDialog(null,
                                         "What year to end?",
                                         "SNOWFALL Data", JOptionPane.QUESTION_MESSAGE);
                         end = Integer.parseInt(inputString);
+
+                        // is valid input
+                        while (!inRangeYear(end))
+                                end = isInvalidResponseYear();
+
+                        // ask user for next input
                         inputString = JOptionPane.showInputDialog(null,
                                         "What month to look at?\n[1-12] Jan-Dec",
                                         "SNOWFALL Data", JOptionPane.QUESTION_MESSAGE);
                         month = Integer.parseInt(inputString);
+
+                        // is valid input
+                        while (!inRangeYear(start))
+                                month = isInvalidResponseMonth();
                         monthlySnow = weather.monthlySnowfall(start, end, month);
                         for (int i = 0; i < monthlySnow.length; i++) {
-                                System.out.println("SNOW AVERAGE FOR " + (start + i) + ": "
+                                System.out.println("SNOWFALL FOR " + (start + i) + ": "
                                                 + String.format("%.3f", monthlySnow[i]));
                         }
                 } // end monthlyRain possible loop
 
                 // Yearly Rainfall Data
                 else if (input == 3) {
+                        // ask user for next input
                         inputString = JOptionPane.showInputDialog(null,
                                         "What year to start?",
                                         "YEARLY RAINFALL Data", JOptionPane.QUESTION_MESSAGE);
                         start = Integer.parseInt(inputString);
 
+                        // is valid input
+                        while (!inRangeYear(start))
+                                start = isInvalidResponseYear();
+
+                        // ask user for next input
                         inputString = JOptionPane.showInputDialog(null,
                                         "What year to end?",
                                         "YEARLY RAINFALL Data", JOptionPane.QUESTION_MESSAGE);
                         end = Integer.parseInt(inputString);
+
+                        // is valid input
+                        while (!inRangeYear(start))
+                                end = isInvalidResponseYear();
+
+                        // final printout
                         yearlyRain = weather.yearlyRainfall(start, end);
                         for (int i = 0; i < yearlyRain.length; i++) {
-                                System.out.println("YEARLY RAINFALL AVERAGE FOR " + (start + i) + ": "
+                                System.out.println("YEARLY RAINFALL FOR " + (start + i) + ": "
                                                 + String.format("%.3f", yearlyRain[i]));
                         }
 
@@ -102,19 +169,57 @@ public class UseWeather {
 
                 // Yearly Snowfall Data
                 else if (input == 4) {
+                        // ask user for next input
                         inputString = JOptionPane.showInputDialog(null,
                                         "What year to start?",
                                         "YEARLY SNOWFALL DATA", JOptionPane.QUESTION_MESSAGE);
                         start = Integer.parseInt(inputString);
 
+                        // is valid input
+                        while (!inRangeYear(start))
+                                start = isInvalidResponseYear();
+                        // ask user for next input
                         inputString = JOptionPane.showInputDialog(null,
                                         "What year to end?",
                                         "YEARLY SNOWFALL DATA", JOptionPane.QUESTION_MESSAGE);
                         end = Integer.parseInt(inputString);
+
+                        // is valid input
+                        while (!inRangeYear(end))
+                                end = isInvalidResponseYear();
+
                         yearlySnow = weather.yearlySnowfall(start, end);
                         for (int i = 0; i < yearlySnow.length; i++) {
-                                System.out.println("YEARLY SNOWFALL AVERAGE FOR " + (start + i) + ": "
+                                System.out.println("YEARLY SNOWFALL FOR " + (start + i) + ": "
                                                 + String.format("%.3f", yearlySnow[i]));
+                        }
+
+                } // end yearlySnowfall possible loop
+
+                // Yearly High Temperature Data
+                else if (input == 5) {
+                        // ask user for next input
+                        inputString = JOptionPane.showInputDialog(null,
+                                        "What year to start?",
+                                        "YEARLY HIGH TEMP DATA", JOptionPane.QUESTION_MESSAGE);
+                        start = Integer.parseInt(inputString);
+
+                        // is valid input
+                        while (!inRangeYear(start))
+                                start = isInvalidResponseYear();
+                        // ask user for next input
+                        inputString = JOptionPane.showInputDialog(null,
+                                        "What year to end?",
+                                        "YEARLY HIGH TEMP DATA", JOptionPane.QUESTION_MESSAGE);
+                        end = Integer.parseInt(inputString);
+
+                        // is valid input
+                        while (!inRangeYear(start))
+                                start = isInvalidResponseYear();
+                        anualHighTemp = weather.yearlyHighTemp(start, end);
+                        for (int i = 0; i < anualHighTemp.length; i++) {
+                                System.out.println("YEARLY HIGH TEMP FOR " + (start + i) + ": "
+                                                + String.format("%.3f", anualHighTemp[i]) + "⁰" + "F");
                         }
 
                 } // end yearlySnowfall possible loop
@@ -126,4 +231,45 @@ public class UseWeather {
                 System.exit(0);
         }
 
+        // ensures that the program will not continue until a valid input has been
+        // received
+        private static int isInvalidResponseYear() {
+                int input; // holds the user's response in an int
+                String inputString; // holds the user's response in a string
+
+                // was the user's input a valid year
+                inputString = JOptionPane.showInputDialog(null,
+                                "That input was invalid, \n please enter a valid number",
+                                "Weather data", JOptionPane.QUESTION_MESSAGE);
+                input = Integer.parseInt(inputString);
+                return input;
+        }// end isInvalidResponse()
+
+        // ensures that the program will not continue until a valid input has been
+        // received
+        private static int isInvalidResponseMonth() {
+                int input; // holds the user's response in an int
+                String inputString; // holds the user's response in a string
+
+                // was the user's input a valid year
+                inputString = JOptionPane.showInputDialog(null,
+                                "That input was invalid, \nplease enter a valid number",
+                                "Weather data", JOptionPane.QUESTION_MESSAGE);
+                input = Integer.parseInt(inputString);
+                return input;
+        }// end isInvalidResponse()
+
+        // checks whether or not the user's input is within the year range
+        private static boolean inRangeYear(int user) {
+                if (user < YEAR_START || user > YEAR_END)
+                        return false;
+                return true;
+        }
+
+        // checks whether or not the user's input is within the year range
+        private static boolean inRangeMonth(int user) {
+                if (user < MONTH_START || user > MONTH_END)
+                        return false;
+                return true;
+        }
 }
--- Weather.java
+++ Weather.java
@@ -2,12 +2,12 @@
 ///////////////////////////////////////////////////////
 // Name: Thomas Gillis
 // 
-// Date: 1/18/2
+// Date: 2/22/22
 //
 // Methods: 
 //      Weather() - Constructor method
 //      monthlyRainfall() - double[] - calculates the 
-//                          rainfall for a particluar 
+//                          rainfall for a particular 
 //                          month over the course of 
 //                          an unknown amount of years 
 //
@@ -17,11 +17,11 @@
 //                          determined span of time
 //      
 //      yearlyRainfall() - double[] - calculates the 
-//                         anual rainfall for a user 
+//                         annual rainfall for a user 
 //                         determined span of time
 //
 //      yearlySnowfall() - double[] - calculates the
-//                         anual snowfall for a user
+//                         annual snowfall for a user
 //                         determined span of time
 //
 //      yearlyHighTemp() - double[] - calculates the
@@ -47,11 +47,11 @@ public class Weather {
     private final int RAINFALL = 5; // denomination for 'rainfall' data in the 'weatherData' array
     private final int SNOWFALL = 6; // denomination for 'snowfall' data in the 'weatherData' array
     private final int SNOW_DEPTH = 7; // denomination for 'snowDepth' data in the 'weatherData' array
-    private double addition; // holds the anual totals before they are entered into the array
+    private double addition; // holds the annual totals before they are entered into the array
     private int difference; // span of time between teh start year and the end year
 
     public double weatherData[][]; // 2D array of doubles that holds the weather data entered by the user
-    public double temp[]; // 1D array of doubles that contains the anual totals for the weather data
+    public double temp[]; // 1D array of doubles that contains the annual totals for the weather data
                           // entered by the user
 
     // creates the necessary variables and arrays for the possible questions
@@ -86,27 +86,20 @@ public class Weather {
                     System.out.println("Not Enough Tokens: " + inputString);
                     System.exit(-1);
                 }
-
-                // you can do the two statements below 8 times and place next into your weather
-                // (1D || 2D) instance varables for later retrieval. You may use all doubles
-                // here since no info is lost when converting an int to a double!
-
                 for (cols = 0; cols < DATA_CATS; cols++) {
                     nextInput = tokens.nextToken(); // extracts next string from tokens object
                     next = Double.parseDouble(nextInput); // converts it!
                     if (rows < ARRAY_SIZE && cols < DATA_CATS) {
                         if (cols < DATA_CATS) {
                             weatherData[rows][cols] = next;
-                        }
-                    } // end if
-                } // end cols for
-            } // end rows for
+                        } // end if statement
+                    } // end if statement
+                } // end cols for loop
+            } // end rows for loop
         } // end of the input loop
-        System.out.println("\nDONE INPUTTING...");
-        // weatherData = new String[ARRAY_SIZE][DATA_CATS];
-    }
+    } // end Weather()
 
-    // calculates the rainfall for a particluar month over the course of an unknown
+    // calculates the rainfall for a particular month over the course of an unknown
     // amount of years
     public double[] monthlyRainfall(int start, int end, int month) {
         resetData();
@@ -115,6 +108,7 @@ public class Weather {
         addition = 0;
         int k = 0;
         int index = 0;
+        // array of doubles get filled to the correct amount
         for (int i = 0; i < difference; i++) {
             for (index = index + 1; index < ARRAY_SIZE
                     && weatherData[index][YEAR] == weatherData[index + 1][YEAR]; index++) {
@@ -125,15 +119,14 @@ public class Weather {
                     // System.out.format("%.3f", addition);
                     // System.out.println("");
                     // System.out.println(addition);
-                } // end month if
-            } // end rows 4
+                } // end month if statement
+            } // end rows for loop
             temp[i] = addition;
             addition = 0;
-        }
+        } // end fill array loop
 
         return temp;
-
-    }
+    } // end monthlyRainfall()
 
     // calculates the snowfall for a particular month over the coure of a user
     // determined span of time
@@ -158,11 +151,11 @@ public class Weather {
             } // end rows 4
             temp[i] = addition;
             addition = 0;
-        }
+        } // end fill array loop
         return temp;
-    }
+    } // end monthlySnowfall()
 
-    // calculates the anual rainfall for a user determined span of time
+    // calculates the annual rainfall for a user determined span of time
     public double[] yearlyRainfall(int start, int end) {
         resetData();
         int difference = (end - start) + 1;
@@ -179,12 +172,12 @@ public class Weather {
             }
             temp[index2] = addition;
             index2++;
-        }
+        } // end fill array loop
 
         return temp;
-    }
+    } // end yearlyRainfall()
 
-    // calculates the anual snowfall for a a user determined span of time
+    // calculates the annual snowfall for a a user determined span of time
     public double[] yearlySnowfall(int start, int end) {
         resetData();
         int difference = (end - start) + 1;
@@ -201,14 +194,34 @@ public class Weather {
             }
             temp[index2] = addition;
             index2++;
-        }
+        } // end fill array loop
         return temp;
-    }
+    } // end yearlySnowfall()
 
-    // calculates the anual high temp during a user determined span of time
+    // calculates the annual high temp during a user determined span of time
     public double[] yearlyHighTemp(int start, int end) {
+        resetData();
+        int difference = (end - start) + 1;
+        int index = 0;
+        int index2 = 0;
+        double maxTemp;
+
+        addition = 0;
+        temp = new double[difference];
+        maxTemp = weatherData[0][HIGH_TEMP];
+        while (index2 < difference) {
+            maxTemp = weatherData[0][HIGH_TEMP];
+            addition = 0;
+            for (index += 1; index < ARRAY_SIZE - 1
+                    && weatherData[index][YEAR] == weatherData[index + 1][YEAR]; index++) {
+                if (weatherData[index][HIGH_TEMP] > maxTemp)
+                    maxTemp = weatherData[index][HIGH_TEMP];
+            }
+            temp[index2] = maxTemp;
+            index2++;
+        } // end fill array loop
         return temp;
-    }
+    } // end yearlyHighTempß
 
     // private method that resets the variables in the class
     private void resetData() {
@@ -216,5 +229,5 @@ public class Weather {
         // weatherData = null;
         temp = null;
 
-    }
+    } // end resetData()
 }
